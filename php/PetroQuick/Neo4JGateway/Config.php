89d351874c295e9cac2af80b91a4e98881361627
Philip Rud <philip.rud@gmail.com>
2010-02-22 22:59:55 +0200

    prebuild war with neo4j-gateway

--- php/PetroQuick/Neo4JGateway/Config.php
+++ php/PetroQuick/Neo4JGateway/Config.php
@@ -3,7 +3,7 @@
 interface PetroQuick_Neo4JGateway_Config {
     const DBSCHEMA_NAMESPACE = 'PetroQuick_Model';
 
-    const GATEWAY_URL = 'http://localhost:8080/JavaBridge/java/Java.inc';
-    const GATEWAY_URL_TEST = 'http://localhost:8080/JavaBridge/java/Java.inc';
+    const GATEWAY_URL = 'http://localhost:8080/neo4j/java/Java.inc';
+    const GATEWAY_URL_TEST = 'http://localhost:8080/neo4j/java/Java.inc';
     const GATEWAY_CONTEXT_PARAM = 'NEO4J_PHPGATEWAY_INSTANCE';
 }
